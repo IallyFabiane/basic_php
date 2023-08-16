4670661d9cdd4ff669afbbf50dc170938fe93409
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-16 15:40:45 -0300

    exercícios php

---
+++ ex00/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo "Hello world, \u{1F30E}!";
+        ?>
+    </p>
+</body>
+</html>
