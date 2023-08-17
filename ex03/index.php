f5e20a6768813ddf4f9e86d6d5575aa1fe591462
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-17 11:36:29 -0300

    interpolação com constantes

---
+++ ex03/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constantes com string</title>
+</head>
+<body>
+    <h1>Interpolação de constantes com string</h1>
+    <?php 
+        const NOME = "Ially";
+        echo "Meu nome é " . NOME;
+    ?>
+</body>
+</html>
