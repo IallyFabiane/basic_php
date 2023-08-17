443e85779d372727b7b576c978c1374ce912d954
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-16 21:40:01 -0300

    ex02

---
+++ ex02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+<body>
+    <h1>Tipos Primitivos</h1>
+    <?php 
+        $num = 300;
+        echo $num . "<br>";
+        $num2 = 0x1A; //Ox= hexadecimal; 0b = binário; 0 = octal;
+        echo $num2. "<br>";
+
+        $v = (int) "300"; //coerção
+        var_dump($v); // informações sobre a variável
+    ?>
+</body>
+</html>
