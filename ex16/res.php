106835e2fe02f19285aedd7255563da65f18907d
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-23 08:27:07 -0300

    formatação lib intl php para moedas

--- ex16/res.php
+++ ex16/res.php
@@ -10,9 +10,12 @@
     <main>
         <h1>Conversor de Moedas v1.0</h1>
         <?php 
-            $moeda = $_GET['moeda'];
-            $dolar = $moeda / 4.9;
-            echo "<p>Seus R$ $moeda equivalem a <strong>$dolar U$ dólares.</strong></p>";
+            $real = $_GET['moeda'];
+            $dolar = $real / 4.9;
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            $cotacaoReal = numfmt_format_currency($padrao, $real, "BRL");
+            $cotacaoDolar = numfmt_format_currency($padrao, $dolar, "USD");
+            echo "<p>Seus R$ $cotacaoReal equivalem a <strong>$cotacaoDolar dólares.</strong></p>";
             echo "<p><small>*Cotação fixa de R$ 4.90.</small></p>";
         ?>  
         <button type="submit"><a href="javascript:history.back()">&#x2B05; Voltar</a></button>
