c7010686b90f7a8fc0066b839b37a87d71add5e9
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-10-08 11:56:20 -0300

    sobrescrita de constutor php

--- ex45/index.php
+++ ex45/index.php
@@ -4,7 +4,8 @@
     require_once 'classes/Veiculo.php';
     require_once 'classes/Carro.php';
 
-    $carro1 = new Carro('carro', 'bmw', 5);
+    $carro1 = new Carro('carro', 'bmw', 5, 4);
     echo "O veículo " . $carro1->getNome() . " suporta " . $carro1->getPassageiros() . " passageiros.\n";
+    echo "Ele tem {$carro1->getRodas()} rodas.\n";
     $carro1->acelerar();
 ?>
