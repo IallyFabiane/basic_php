2f0e3ce91eb8a52d9025611d0fc2ab78c329623c
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-10-10 21:47:48 -0300

    session functions e cookie de sessão

--- ex36/home.php
+++ ex36/home.php
@@ -1,4 +1,6 @@
 <?php 
-    session_start();
+    if (session_status() !== PHP_SESSION_ACTIVE) session_start(); //SE NENHUMA SESSÃO ESTIVER ATIVA, INICIA UMA SESSÃO
+   // Acesse o valor do cookie de sessão genérico
+    $sessionId = $_COOKIE['PHPSESSID'];
     echo $_SESSION['cor'] . "<br>" . $_SESSION['carro'] . "<br>" . session_id(); 
 ?>
--- ex36/logout.php
+++ ex36/logout.php
@@ -1,5 +1,5 @@
 <?php 
     session_start(); //iniciando a sessão
-    session_unset(); //limpando a sessão
-    session_destroy(); //finalizando a sessão
+    session_unset(); //limpando a sessão OU $_SESSION = array();
+    session_destroy(); //finalizando a sessão, pra deslogar o usuário, antes de usar o destroy temos que usar o unset
 ?>
